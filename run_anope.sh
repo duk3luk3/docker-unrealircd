26947c2e986e965b6555fdd29bbd3f3d952a35c7
Michael Sondergaard <sheeo@faforever.com>
2017-01-23 07:19:36 -0600

    Make database server runtime setting

--- run_anope.sh
+++ run_anope.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SECRET_MYSQL_SERVER="${FAF_DB_1_PORT_3306_TCP_ADDR}"
+sed -i "s/%SECRET_MYSQL_SERVER%/${SECRET_MYSQL_SERVER}/" /home/unreal/unrealircd/services/conf/services.conf
+
 while true
 do
 if pgrep -f services 1> /dev/null;then
