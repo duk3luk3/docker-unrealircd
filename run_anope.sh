01599de9e9ea6ffef6c224c81b6396a42fdbd813
Sheeo <sheeo@sheeo.dk>
2017-10-09 19:19:28 +0200

    Bring back silly anope infinite run-loop
    This is needed until we bring anope into its own container so docker can
    healthcheck and restart it independently of the unrealircd daemon.
    Otherwise anope crashes will bring the entire irc server down

--- run_anope.sh
+++ run_anope.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-
-/home/unreal/unrealircd/services/bin/services -nofork -debug
+hile true
+do
+if pgrep -f services 1> /dev/null;then
+	sleep 1
+else
+        echo SERVER CRASHED
+        /home/unreal/unrealircd/services/bin/services -nofork -debug
+fi
+done
